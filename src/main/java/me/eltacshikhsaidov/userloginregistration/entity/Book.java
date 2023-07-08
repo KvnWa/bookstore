ed5c063a655645086a2b574463743f146d12a4c0
Kevin Wang <kevqwang@gmail.com>
2023-07-08 10:40:45 -0400

    tracking

---
+++ src/main/java/me/eltacshikhsaidov/userloginregistration/entity/Book.java
@@ -0,0 +1,65 @@
+package me.eltacshikhsaidov.userloginregistration.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+public class Book {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "book_url")
+    private String bookUrl;
+
+    @Column(name = "author")
+    private String author;
+
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usr_id")
+    private User publisher;
+
+    // creating constructor without id
+    public Book(String title, String description, String imageUrl, String author, String bookUrl, User publisher) {
+        this.title = title;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.bookUrl = bookUrl;
+        this.author = author;
+        this.publisher = publisher;
+    }
+
+    public Book(String title, String description, String imageUrl, String bookUrl, String author) {
+        this.title = title;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.bookUrl = bookUrl;
+        this.author = author;
+    }
+}
---
+++ src/main/java/me/eltacshikhsaidov/userloginregistration/service/RegistrationService.java
@@ -0,0 +1,85 @@
+package me.eltacshikhsaidov.userloginregistration.service;
+
+import lombok.AllArgsConstructor;
+import me.eltacshikhsaidov.userloginregistration.entity.User;
+import me.eltacshikhsaidov.userloginregistration.entity.role.UserRole;
+import me.eltacshikhsaidov.userloginregistration.service.sender.EmailSender;
+import me.eltacshikhsaidov.userloginregistration.service.token.ConfirmationToken;
+import me.eltacshikhsaidov.userloginregistration.service.validator.EmailValidator;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+
+    private final UserService userService;
+    private final EmailValidator emailValidator;
+    private final ConfirmationTokenService confirmationTokenService;
+    private final EmailSender emailSender;
+
+    public String register(RegistrationRequest request, UserRole userRole) {
+        boolean isValidEmail = emailValidator.
+                test(request.getEmail());
+
+        if (!isValidEmail) {
+            // throw new IllegalStateException("email not valid");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "email not valid");
+        }
+
+        String token = userService.signUpUser(
+                new User(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        userRole
+                )
+        );
+
+        String link = "http://localhost:8080/api/register/confirm?token=" + token;
+        emailSender.send(
+                request.getEmail(),
+                buildEmail(request.getFirstName(), link));
+
+        return token;
+    }
+
+    @Transactional
+    public String confirmToken(String token) {
+        ConfirmationToken confirmationToken = confirmationTokenService
+                .getToken(token)
+                .orElseThrow(() ->
+                        // new IllegalStateException("token not found"));
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "token not found"));
+
+        if (confirmationToken.getConfirmedAt() != null) {
+            // throw new IllegalStateException("email already confirmed");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "email already confirmed");
+        }
+
+        LocalDateTime expiredAt = confirmationToken.getExpiresAt();
+
+        if (expiredAt.isBefore(LocalDateTime.now())) {
+            // throw new IllegalStateException("token expired");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "token expired");
+        }
+
+        confirmationTokenService.setConfirmedAt(token);
+        userService.enableAppUser(
+                confirmationToken.getUser().getEmail());
+        return "confirmed";
+    }
+
+    private String buildEmail(String name, String link) {
+
+        return "<p>Hi " 
+                    + name + ". Please confirm your email with <a href=" 
+                    + link + " taget=\"_blank\">link</a>.It will expire after 15 minutes. Hurry up!</p>";
+    }
+}
